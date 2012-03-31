6e51e1eac22aa9420cbf26ab574505e698ef029a
shakti.sd <shakti.sd@gmail.com>
2012-03-31 08:42:30 +0000

    RC 1 with Ad mob

--- NeverLost/src/com/phonecop/constants/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.phonecop.constants;
-
-public class Constants {
-	public static final String PHONENUMBER = "PHONENUMBER";
-	public static final String ALARMTEXT = "ALARMTEXT";
-	public static final String ALARMONSMSFROMANYNUMBER = "ALARMONSMSFROMANYNUMBER";
-}
