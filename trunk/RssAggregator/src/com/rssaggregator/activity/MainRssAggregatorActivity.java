bcbceb2fc4030ec5371fe56e4876ded188f7602a
shakti.sd@gmail.com <shakti.sd@gmail.com@4d30bfd3-ea45-5beb-db81-74dbf01a7bc2>
2012-04-14 03:41:51 +0000

    1. Added notification on download 2. Aligned AdView to bottom center 3. Removed menu refresh from Feeds screen

--- trunk/RssAggregator/src/com/rssaggregator/activity/MainRssAggregatorActivity.java
+++ trunk/RssAggregator/src/com/rssaggregator/activity/MainRssAggregatorActivity.java
@@ -1,11 +1,15 @@
 package com.rssaggregator.activity;
 
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import android.app.ExpandableListActivity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
@@ -67,10 +71,6 @@ public class MainRssAggregatorActivity extends ExpandableListActivity {
 	}
 	
 	public boolean onChildClick(ExpandableListView parent, View v, int groupPosition,int childPosition, long id) {
-		if (!rssAggregatorApplication.isOnline()){
-			Toast.makeText(this, "No INTERNET connection detected", Toast.LENGTH_LONG).show();
-			return true;
-		}
 		HashMap<String,String> feedSourceDetails = (HashMap<String,String>)mAdapter.getChild(groupPosition, childPosition);
 		String feedSource = (String)feedSourceDetails.values().iterator().next();
 		rssAggregatorApplication.setFeedSourceName(feedSource);
@@ -91,44 +91,76 @@ public class MainRssAggregatorActivity extends ExpandableListActivity {
 		return true;
 	}
 
-	/**
-	 * Event Handling for Individual menu item selected Identify single menu
-	 * item by it's id
-	 * */
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case R.id.menu_refresh:
-			if (!rssAggregatorApplication.isOnline()){
-				Toast.makeText(this, "No INTERNET connection detected", Toast.LENGTH_LONG).show();
-				return true;
-			}
-			 Toast.makeText(this, "Refresh started", Toast.LENGTH_SHORT).show();
-			new DownloadFilesTask().execute(getApplicationContext());
-			return true;
-		/*case R.id.menu_add_rss_source:
-			Intent intent = new Intent(this, FeedSourceActivity.class);
-			startActivity(intent);
-			return true;	*/		
-		default:
-			return super.onOptionsItemSelected(item);
+
+
+	private class DownloadFilesTask extends AsyncTask<Context, String, String> {
+		NotificationManager notificationManager;
+		int icon;
+		String tickerText;
+		long time;
+		Notification notification;
+		String contentTitle;
+		String contentText;
+		PendingIntent contentIntent;
+		int HELLO_ID = 1;
+
+		@Override
+		protected void onPreExecute() {
+			notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+			icon = R.drawable.readrss;
+			// the text that appears first on the status bar
+			tickerText = "Downloading...";
+			time = System.currentTimeMillis();
+			notification = new Notification(icon, tickerText, time);
+			// the bold font
+			contentTitle = "RSS Feed download in progress";
+			// the text that needs to change
+			contentText = "0% complete";
+			Intent notificationIntent = new Intent(Intent.ACTION_VIEW);
+			contentIntent = PendingIntent.getActivity(getApplicationContext(), 0, notificationIntent, 0);
+			notification.setLatestEventInfo(getApplicationContext(), contentTitle, contentText, contentIntent);
+			notificationManager.notify(HELLO_ID, notification);
+
+			super.onPreExecute();
 		}
-	}
-	
-	 private class DownloadFilesTask extends AsyncTask<Context, String, String> {
-	     protected String doInBackground(Context... context) {
-	    	 refreshRssFeeds();
-	    	 return null;
-	     }
+
+		@Override
+		protected String doInBackground(Context... context) {
+
+			Log.i("RSSAGGREGATOR", "Refreshing feeds ");
+			List<FeedSource> allFeedSources = rssAggregatorApplication.findAllFeedSource();
+			int totalFeeds = allFeedSources.size();
+			int storedFeeds = 0;
+			publishProgress(storedFeeds + "/" + totalFeeds);
+			for (FeedSource feedSource : allFeedSources) {
+				List<FeedSource> listFeedSource = new ArrayList<FeedSource>();
+				listFeedSource.add(feedSource);
+				List<RssFeed> rssFeeds = rssAggregatorApplication.getRssFeeds(listFeedSource);
+				rssAggregatorApplication.storeFeeds(rssFeeds);
+				storedFeeds++;
+				publishProgress(storedFeeds + "/" + totalFeeds);
+			}
+			Log.i("RSSAGGREGATOR", "Refresh complete");
+
+			return null;
+		}
 
 		@Override
 		protected void onPostExecute(String result) {
 			Toast.makeText(getApplicationContext(), "Refresh complete", Toast.LENGTH_SHORT).show();
-			showCategories();
+			notificationManager.cancel(HELLO_ID);
+			// notificationManager.notify(HELLO_ID, notification);
 		}
-	     
-	     
-	 }
+
+		@Override
+		public void onProgressUpdate(String... progress) {
+			contentText = progress[0] + " complete";
+			notification.setLatestEventInfo(getApplicationContext(), contentTitle, contentText, contentIntent);
+			notificationManager.notify(HELLO_ID, notification);
+			super.onProgressUpdate(progress);
+		}
+
+	}
 
 	
 	private void showCategories(){
@@ -161,6 +193,30 @@ public class MainRssAggregatorActivity extends ExpandableListActivity {
 				new int[] { R.id.customrowtext1});
 		setListAdapter(mAdapter);
 
+	}
+	
+	/**
+	 * Event Handling for Individual menu item selected Identify single menu
+	 * item by it's id
+	 * */
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.menu_refresh:
+			if (!rssAggregatorApplication.isOnline()){
+				Toast.makeText(this, "No INTERNET connection detected", Toast.LENGTH_LONG).show();
+				return true;
+			}
+			Toast.makeText(this, "Refresh started", Toast.LENGTH_SHORT).show();
+			new DownloadFilesTask().execute(getApplicationContext());
+			return true;
+		/*case R.id.menu_add_rss_source:
+			Intent intent = new Intent(this, FeedSourceActivity.class);
+			startActivity(intent);
+			return true;	*/		
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
 	
 	private void refreshRssFeeds() {
