7fba9db2e374cedc983e3fd180e71b6fd75a82b6
shakti.sd <shakti.sd@gmail.com>
2012-05-02 16:13:31 +0000

    Add / Delete FeedSource & Category

--- RssAggregator/src/com/rssaggregator/activity/FeedSourceActivity.java
+++ RssAggregator/src/com/rssaggregator/activity/FeedSourceActivity.java
@@ -6,10 +6,15 @@ import java.util.List;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.rssaggregator.valueobjects.Category;
 import com.rssaggregator.valueobjects.FeedSource;
@@ -20,6 +25,11 @@ public class FeedSourceActivity extends Activity {
 	TextView feedSourceURL;
 	Button feedSourceSave;
 	Button feedSourceDelete;
+	ListView feedSourceListView;
+	ArrayAdapter<String> adapter;
+	Category currentCategory ;
+	List<String> values ;
+	ArrayAdapter<String> dataAdapter;
 	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -32,24 +42,141 @@ public class FeedSourceActivity extends Activity {
 	    for(Category category : allCategory){
 	    	list.add(category.getCategoryName());
 	    }
-		ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, list);
+		dataAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, list);
 	    dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 	    spinner.setAdapter(dataAdapter);
+	    
+	    values = getAllFeedSourceByCategoryName(String.valueOf(spinner.getSelectedItem()));
+	    adapter = new ArrayAdapter<String>(this,R.layout.list_item,R.id.customitemtext1, values);
+		// Assign adapter to ListView
+	    feedSourceListView.setAdapter(adapter);
 	}
 	
+	private List<String> getAllFeedSourceByCategoryName(String categoryName) {
+		this.currentCategory = getRssAggregatorApplication().findCategoryByName(categoryName);
+		List<String> values = new ArrayList<String>();
+		for(FeedSource feedSource : currentCategory.getFeedSources()){
+			values.add(feedSource.getFeedSourceName());
+		}
+		return values;
+	}
+
 	private void registerClickListenersToButtons() {
 		feedSourceSave.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
+				String message = "";
 				String selectedCategory = String.valueOf(spinner.getSelectedItem());
 				String name = feedSourceName.getText().toString();
 				String url = feedSourceURL.getText().toString();
+				message = validate(name,url);
+				FeedSource feedSource = new FeedSource(name,url);
+				if (message.length() == 0){
+					Category storedCategory = getRssAggregatorApplication().findCategoryByName(selectedCategory);
+					if ( storedCategory.getFeedSources() == null ){
+						storedCategory.setFeedSources(new ArrayList<FeedSource>());
+					}
+					
+					FeedSource storedFeedSource = getRssAggregatorApplication().findFeedSourceByName(feedSource);
+					
+					if(storedFeedSource == null){
+						storedCategory.getFeedSources().add(feedSource);
+						getRssAggregatorApplication().save(storedCategory);
+						values.add(name);
+						adapter.notifyDataSetChanged();	
+						feedSourceName.setText("");
+						feedSourceURL.setText("");
+						message = "Saved ";
+						
+					}else {
+						message = "FeedSource already exists ";
+					}	
+				}
 				
-				Category categoryByName = getRssAggregatorApplication().findCategoryByName(selectedCategory);
-				categoryByName.getFeedSources().add(new FeedSource(name,url));
-				getRssAggregatorApplication().save(categoryByName);
+				
+				Toast.makeText(getApplicationContext(), message + feedSource.getFeedSourceName() , Toast.LENGTH_SHORT).show();
+				
+			}
+
+			private String validate(String name, String url) {
+				StringBuffer message = new StringBuffer();
+				if(name == null){
+					message.append(" Provide name");
+				}
+				
+				if(name != null && name.trim().length() == 0){
+					message.append(" Provide name");
+				}
+				
+				if(url == null){
+					message.append(" Provide url");
+				}
+				
+				if(url != null && url.trim().length() == 0){
+					message.append(" Provide url");
+				}
+				
+				return message.toString();
+				
 			}
 		});
+		
+		feedSourceDelete.setOnClickListener(new View.OnClickListener(){
+			private RssAggregatorApplication rssAggregatorApplication;
+
+			@Override
+			public void onClick(View v) {
+				FeedSource queryFeedSource = new FeedSource(feedSourceName.getText().toString(),feedSourceURL.getText().toString());
+				rssAggregatorApplication = getRssAggregatorApplication();
+				FeedSource storedFeedSource = rssAggregatorApplication.findFeedSourceByName(queryFeedSource);
+				StringBuffer message = new StringBuffer();
+				if (storedFeedSource == null){
+					message.append("FeedSource : " + queryFeedSource.getFeedSourceName() + " doen't exists");
+				}else {
+					Category categoryByName = rssAggregatorApplication.findCategoryByName(String.valueOf(spinner.getSelectedItem()));
+					rssAggregatorApplication.deleteCategoryFeedSourceAndFeeds(categoryByName,storedFeedSource);
+					values.remove(storedFeedSource.getFeedSourceName());
+					adapter.notifyDataSetChanged();
+					message.append("Deleted FeedSource : " + storedFeedSource.getFeedSourceName());
+					feedSourceName.setText("");
+					feedSourceURL.setText("");
+				}
+				Toast.makeText(getApplicationContext(), message.toString(), Toast.LENGTH_SHORT).show();
+			
+			}
+			
+		});
+		
+		spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+		    @Override
+		    public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+		    	values.clear();
+		    	values.addAll( getAllFeedSourceByCategoryName(String.valueOf(spinner.getSelectedItem()))) ;
+		    	adapter.notifyDataSetChanged();
+		    }
+
+		    @Override
+		    public void onNothingSelected(AdapterView<?> parentView) {
+		        // your code here
+		    }
+
+		});
+		
+		feedSourceListView.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View view, int position, long arg3) {
+				String selectedText = (((TextView)view).getText()).toString();
+				for(FeedSource feedSource : currentCategory.getFeedSources()){
+					if(feedSource.getFeedSourceName().equalsIgnoreCase(selectedText)){
+						feedSourceName.setText(feedSource.getFeedSourceName());
+						feedSourceURL.setText(feedSource.getFeedSourceUrl());
+					}
+				}
+		
+			}
+		});
+
+		
 	}
 
 	private void registerViewItems() {
@@ -58,6 +185,7 @@ public class FeedSourceActivity extends Activity {
 		feedSourceURL = (TextView) findViewById(R.id.feedsourceurltext);
 		feedSourceSave = (Button) findViewById(R.id.saveFeedSource);
 		feedSourceDelete = (Button) findViewById(R.id.deleteFeedSource);
+		feedSourceListView =  (ListView) findViewById(R.id.feedsourcelist);
 	}
 
 	private RssAggregatorApplication getRssAggregatorApplication() {
